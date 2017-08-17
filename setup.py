71849c439606e5eca46802e4fb7f7a91601a2cca
wiheto <whthompson@ymail.com>
2017-08-17 21:14:25 +0200

    author email added

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='Teneto',
       description='Temporal network tools',
       packages=find_packages(),
       author='William Hedley Thompson',
+      author_email='hedley@startmail.com',
       url='https://www.github.com/wiheto/teneto',
       download_url='https://github.com/wiheto/teneto/archive/0.2.tar.gz'
       )
