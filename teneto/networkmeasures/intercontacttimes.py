1da952435032357b88906484b55bee871b0bc237
William Hedley <whthompson@ymail.com>
2017-08-21 15:17:57 +0200

    made code more readable

--- teneto/networkmeasures/intercontacttimes.py
+++ teneto/networkmeasures/intercontacttimes.py
@@ -1,5 +1,9 @@
+"""
+networkmeasures: intercontacttimes
+"""
+
 import numpy as np
-from teneto.utils import *
+import teneto.utils as utils
 
 
 def intercontacttimes(netin):
@@ -14,7 +18,7 @@ def intercontacttimes(netin):
 
     **OUTPUT**
 
-    :ICT: intercontact times as numpy array
+    :contacts: intercontact times as numpy array
 
         :format: dictionary
 
@@ -34,34 +38,35 @@ def intercontacttimes(netin):
     """
 
     # Process input
-    netin, netinfo = process_input(netin, ['C', 'G', 'TO'])
+    netin, netinfo = utils.process_input(netin, ['C', 'G', 'TO'])
 
     if netinfo['nettype'][0] == 'd':
         print('WARNING: assuming connections to be binary when computing intercontacttimes')
 
-    # Each time series is padded with a 0 at the start and end. Then t[0:-1]-[t:]. Then discard the noninformative ones (done automatically)
+    # Each time series is padded with a 0 at the start and end. Then t[0:-1]-[t:].
+    # Then discard the noninformative ones (done automatically)
     # Finally return back as np array
-    ICT = np.array([[None] * netinfo['netshape'][0]] * netinfo['netshape'][1])
+    contacts = np.array([[None] * netinfo['netshape'][0]] * netinfo['netshape'][1])
 
     if netinfo['nettype'][1] == 'u':
         for i in range(0, netinfo['netshape'][0]):
             for j in range(i + 1, netinfo['netshape'][0]):
-                Aon = np.where(netin[i, j, :] > 0)[0]
-                Aon = np.append(0, Aon)
-                Aon = np.append(Aon, 0)
-                Aon_diff = Aon[2:-1] - Aon[1:-2]
-                ICT[i, j] = np.array(Aon_diff)
-                ICT[j, i] = np.array(Aon_diff)
+                edge_on = np.where(netin[i, j, :] > 0)[0]
+                edge_on = np.append(0, edge_on)
+                edge_on = np.append(edge_on, 0)
+                edge_on_diff = edge_on[2:-1] - edge_on[1:-2]
+                contacts[i, j] = np.array(edge_on_diff)
+                contacts[j, i] = np.array(edge_on_diff)
     elif netinfo['nettype'][1] == 'd':
         for i in range(0, netinfo['netshape'][0]):
             for j in range(0, netinfo['netshape'][0]):
-                Aon = np.where(netin[i, j, :] > 0)[0]
-                Aon = np.append(0, Aon)
-                Aon = np.append(Aon, 0)
-                Aon_diff = Aon[2:-1] - Aon[1:-2]
-                ICT[i, j] = np.array(Aon_diff)
+                edge_on = np.where(netin[i, j, :] > 0)[0]
+                edge_on = np.append(0, edge_on)
+                edge_on = np.append(edge_on, 0)
+                edge_on_diff = edge_on[2:-1] - edge_on[1:-2]
+                contacts[i, j] = np.array(edge_on_diff)
 
     out = {}
-    out['intercontacttimes'] = ICT
+    out['intercontacttimes'] = contacts
     out['nettype'] = netinfo['nettype']
     return out
