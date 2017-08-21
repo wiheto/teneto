e4e14e8b7559421c340a23e648af3c3518ff6585
William Hedley <whthompson@ymail.com>
2017-08-21 15:04:40 +0200

    code made nicer

--- teneto/derive/report.py
+++ teneto/derive/report.py
@@ -1,17 +1,25 @@
-import matplotlib.pyplot as plt
+"""
+derive.report
+"""
+
 import os
+import matplotlib.pyplot as plt
 import numpy as np
-import csv
 
 
 def gen_report(report, sdir='./'):
+
+    """
+    Generates report of derivation and postprocess steps in teneto.derive
+    """
 
     # Create report directory
     if not os.path.exists(sdir):
         os.makedirs(sdir)
 
-    # Add a slash to file directory if not included to avoid DirNameFleName instead of DirName/FileName being creaated
-    if sdir[-1] is not '/':
+    # Add a slash to file directory if not included to avoid DirNameFleName
+    # instead of DirName/FileName being creaated
+    if sdir[-1] != '/':
         sdir += '/'
 
     report_html = '<html><body>'
@@ -53,15 +61,15 @@ def gen_report(report, sdir='./'):
 
             report_html += "<p><h3>" + i + "</h3><p>"
 
-            for ii in report[i]:
+            for j in report[i]:
 
-                if ii == 'lambda':
+                if j == 'lambda':
 
-                    report_html += "- <b>" + ii + "</b>: " + "<br>"
+                    report_html += "- <b>" + j + "</b>: " + "<br>"
 
-                    l = np.array(report['boxcox']['lambda'])
+                    lambda_val = np.array(report['boxcox']['ambda_valambda'])
                     fig, ax = plt.subplots(1)
-                    ax.hist(l[:, -1])
+                    ax.hist(lambda_val[:, -1])
                     ax.set_xlabel('lambda')
                     ax.set_ylabel('frequency')
                     ax.set_title('Histogram of lambda parameter')
@@ -70,15 +78,15 @@ def gen_report(report, sdir='./'):
                     report_html += "<img src='./boxcox_lambda.png' width=500>" + "<p>"
                     report_html += "Data located in " + sdir + "boxcox_lambda.csv <p>"
 
-                    np.savetxt(sdir + "boxcox_lambda.csv", l, delimiter=",")
+                    np.savetxt(sdir + "boxcox_lambda.csv", lambda_val, delimiter=",")
 
                 else:
 
-                    report_html += "- <b>" + ii + "</b>: " + \
-                        str(report[i][ii]) + "<br>"
+                    report_html += "- <b>" + j + "</b>: " + \
+                        str(report[i][j]) + "<br>"
 
     report_html += '</body></html>'
 
-    with open(sdir + 'derivation_report.html', 'w') as f:
-        f.write(report_html)
-    f.close()
+    with open(sdir + 'derivation_report.html', 'w') as file:
+        file.write(report_html)
+    file.close()
