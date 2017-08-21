956610076f592b6a1d7aaf9692e87a5428b129e3
William Hedley <whthompson@ymail.com>
2017-08-21 13:56:00 +0200

    temporal degree. Code nicer. do -> calc, sumOverDim -> axis

--- teneto/networkmeasures/temporal_degree_centrality.py
+++ teneto/networkmeasures/temporal_degree_centrality.py
@@ -1,15 +1,19 @@
+"""
+Network mesures: temporal degree centrality
+"""
+
 import numpy as np
-from teneto.utils import *
+import teneto.utils as utils
 
 
-def temporal_degree_centrality(netIn, d=0, do='avg', subnetworks=None):
+def temporal_degree_centrality(net_in, axis=0, calc='avg', subnet=None):
     """
 
     temporal degree of network. Sum of all connections each node has through time.
 
     **PARAMETERS**
 
-    :netIn: temporal network input (graphlet or contact).
+    :net_in: temporal network input (graphlet or contact).
 
         :nettype: 'bu', 'bd', 'wu', 'wd'
 
@@ -18,15 +22,18 @@ def temporal_degree_centrality(netIn, d=0, do='avg', subnetworks=None):
         i.e. if 0, node i has Aijt summed over j and t.
         and if 1, node j has Aijt summed over i and t.
 
-    :do: 'avg' (returns temporal degree centrality (a 1xnode vector)) or 'time' (returns a node x time matrix). i.e. 'time' returns static degree centrality per time-point
+    :calc: 'avg' (returns temporal degree centrality (a 1xnode vector))
+     or 'time' (returns a node x time matrix).
+     i.e. 'time' returns static degree centrality per time-point.
 
-    :subnetworks: None (default) or Nx1 vector of subnetwork assignment. This returns a "centrality" per subnetwork instead of per node.  
+    :subnet: None (default) or Nx1 vector of subnetwork assignment.
+    This returns a "centrality" per subnetwork instead of per node.
 
     **OUTPUT**
 
     :D: temporal degree centrality (nodal measure)
 
-        :format: 1d numpy array (or 2d if do = 'time')
+        :format: 1d numpy array (or 2d if calc = 'time')
 
     **SEE ALSO**
 
@@ -34,30 +41,30 @@ def temporal_degree_centrality(netIn, d=0, do='avg', subnetworks=None):
 
     **HISTORY**
 
-    Modified - Mar 2017, WHT (do='time')
-    Modified - Dec 2016, WHT (docmentation)
+    Modified - Mar 2017, WHT (calc='time')
+    Modified - Dec 2016, WHT (calccmentation)
     Created - Nov 2016, WHT
 
     """
 
     # Get input in right format
-    netIn, netInfo = process_input(netIn, ['C', 'G', 'TO'])
+    net_in, network_info = utils.process_input(
+        net_in, ['C', 'G', 'TO'])
 
-    # Set the nodal dimension to sum over to 0, if d==1 and nettype is '.d', this gets changes to 0.
-    sumOverDim = 1
-    # set sumDimension to 0 if nettype is d and user specifcies d=1
-    if d == 1:
-        sumOverDim = 0
-    # sum sum netIn
-    if do == 'time' and subnetworks == None:
-        tDeg = np.squeeze(np.sum(netIn, axis=sumOverDim))
-    elif do != 'time' and subnetworks == None:
-        tDeg = np.sum(np.sum(netIn, axis=2), axis=sumOverDim)
-    elif do == 'time' and subnetworks != None:
+    # sum sum net_in
+    if calc == 'time' and not subnet:
+        tdeg = np.squeeze(np.sum(net_in, axis=axis))
+    elif calc != 'time' and not subnet:
+        tdeg = np.sum(
+            np.sum(net_in, axis=2), axis=axis)
+    elif calc == 'time' and subnet:
+        unique_subnet = np.unique(subnet)
+        tdeg_subnet = [np.sum(np.sum(net_in[subnet == s1, :, :][:, subnet == s2, :], axis=1), axis=0)
+                       for s1 in unique_subnet for s2 in unique_subnet]
+        tdeg = np.array(tdeg_subnet)
+        tdeg = np.reshape(tdeg, [len(np.unique(subnet)), len(
+            np.unique(subnet)), net_in.shape[-1]])
+    else:
+        raise ValueError("invalid calc argument")
 
-        tDeg = np.array([np.sum(np.sum(netIn[subnetworks == sn1, :, :][:, subnetworks == sn2, :], axis=1), axis=0)
-                         for sn1 in np.unique(subnetworks) for sn2 in np.unique(subnetworks)])
-        tDeg = np.reshape(tDeg, [len(np.unique(subnetworks)), len(
-            np.unique(subnetworks)), netIn.shape[-1]])
-
-    return tDeg
+    return tdeg
