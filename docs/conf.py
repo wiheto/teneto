a363a702822d1a4c27b52d14854920a2cd49a058
wiheto <william.thompson@ki.se>
2018-03-25 14:28:22 -0700

    possible readthedocs fix

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ import os
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'numpydoc'
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
