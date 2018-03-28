672e40e07c8d11c84f62d78492fb9513f30deffa
wiheto <william.thompson@ki.se>
2018-03-27 22:21:45 -0700

    added mock modules

--- docs/conf.py
+++ docs/conf.py
@@ -286,3 +286,5 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+MOCK_MODULES = ['numpy', 'pandas','scipy','nilearn','sklearn','pybids']
