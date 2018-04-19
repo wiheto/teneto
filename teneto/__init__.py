0e2e8c2cad1fb640b64401a8c4bd7214d6cfd69b
wiheto <william.thompson@ki.se>
2018-04-19 12:17:04 -0700

    0.3.1 ready

--- teneto/__init__.py
+++ teneto/__init__.py
@@ -4,7 +4,7 @@
 """Teneto is a module with tools for analyzing network patterns in time."""
 #
 __author__ = "William Hedley Thompson (wiheto)"
-__version__ = "0.3.1-develop"
+__version__ = "0.3.1"
 #
 import teneto.utils as utils
 import teneto.networkmeasures as networkmeasures
