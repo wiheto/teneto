ecee460cd80ff877c1c381d7a45e42b20a8a1336
wiheto <william.thompson@ki.se>
2018-05-19 19:07:58 -0700

    shortest path tests

--- teneto/test/utils/basictests.py
+++ teneto/test/utils/basictests.py
@@ -1,6 +1,7 @@
 
 import teneto
 import numpy as np
+import matplotlib.pyplot as plt
 
 def test_graphletconversion():
     # For reproduceability
@@ -25,6 +26,7 @@ def test_networkmeasures_tdc():
     G[1,2,3] = 1
     G += G.transpose([1,0,2]) 
     G = teneto.utils.set_diagonal(G,1)
+    # Call different instances of temporal_degree_centrality 
     C1 = teneto.networkmeasures.temporal_degree_centrality(G)
     C2 = teneto.networkmeasures.temporal_degree_centrality(G,axis=1)
     C3 = teneto.networkmeasures.temporal_degree_centrality(G,axis=1,calc='time')
@@ -39,4 +41,42 @@ def test_networkmeasures_tdc():
     assert (C3 == G.sum(axis=1)).all()
     assert (C4 == tC4).all()
     
-
+def test_networkmeasures_stp(): 
+    # Make simple network
+    G = np.zeros([3,3,4])
+    G[0,1,[0,2,3]] = 1
+    G[0,2,1] = 1
+    G[1,2,3] = 1
+    G += G.transpose([1,0,2]) 
+    G = teneto.utils.set_diagonal(G,1)
+    sp = teneto.networkmeasures.shortest_temporal_path(G)
+    sp['paths'] = teneto.utils.set_diagonal(sp['paths'],0)
+    paths_true = np.zeros(sp['paths'].shape)
+    #reminder dimord is from,to
+    paths_true[0,1,0] = 1
+    paths_true[0,2,0] = 2
+    paths_true[1,0,0] = 1
+    paths_true[1,2,0] = 2
+    paths_true[2,1,0] = 3
+    paths_true[2,0,0] = 2
+    paths_true[0,1,1] = 2
+    paths_true[0,2,1] = 1
+    paths_true[1,0,1] = 2
+    paths_true[1,2,1] = 3
+    paths_true[2,1,1] = 2
+    paths_true[2,0,1] = 1
+    paths_true[0,1,2] = 1
+    paths_true[0,2,2] = 2
+    paths_true[1,0,2] = 1
+    paths_true[1,2,2] = 2
+    paths_true[2,1,2] = 2
+    paths_true[2,0,2] = 2
+    paths_true[0,1,3] = 1
+    paths_true[0,2,3] = 1
+    paths_true[1,0,3] = 1
+    paths_true[1,2,3] = 1
+    paths_true[2,1,3] = 1
+    paths_true[2,0,3] = 1
+    assert (sp['paths'] == paths_true).all()
+    #fig,ax = plt.subplots(1)
+    #ax = teneto.plot.slice_plot(G,ax)
