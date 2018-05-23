eae18037bfaba9f6d6d26dd0d2668eb3d7c89d3b
wiheto <william.thompson@ki.se>
2018-05-22 21:37:48 -0700

    test community detection speedup

--- teneto/test/communitydetection/test_communitydetection.py
+++ teneto/test/communitydetection/test_communitydetection.py
@@ -8,7 +8,7 @@ def test_community():
     b=np.array([[1,1,1,1,0,0],[1,1,1,1,0,0],[1,1,1,1,0,0],[1,1,1,1,0,0],[0,0,0,0,1,1],[0,0,0,0,1,1]])
     # Make into 3 time points 
     G = np.stack([a,a,b,b]).transpose([1,2,0])
-    C = teneto.communitydetection.temporal_louvain_with_consensus(G,iter_n=50,interslice_weight=0)
+    C = teneto.communitydetection.temporal_louvain_with_consensus(G,iter_n=10,interslice_weight=0)
     assert C[0,0] == C[1,0] == C[2,0]
     assert C[3,0] == C[4,0] == C[5,0]
     assert C[0,2] == C[1,2] == C[2,2] == C[3,2]
