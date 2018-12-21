8615c8272be6368e668de15196c6751bdad2ff7b
wiheto <william.thompson@ki.se>
2018-12-21 00:06:28 -0800

    added equations

--- teneto/networkmeasures/topological_overlap.py
+++ teneto/networkmeasures/topological_overlap.py
@@ -12,7 +12,7 @@ def topological_overlap(tnet, calc='time'):
     tnet : array, dict
         graphlet or contact sequence input. Nettype: 'bu'.
     calc: str 
-        which version of topological overlap to calculate
+        which version of topological overlap to calculate: 
         'node' - calculates for each node, averaging over time. 
         'time' - (default) calculates for each node per time points.
         'global' - (default) calculates for each node per time points.
@@ -25,12 +25,31 @@ def topological_overlap(tnet, calc='time'):
         if calc = 'node', array is (node) in size. 
         if calc = 'global', array is (1) in size. 
 
+    Notes 
+    ------
+    When edges persist over time, the topological overlap increases. It can be calculated as a global valu, per node, per node-time. 
+
+    When calc='time', then the topological overlap is:   
+
+    .. math:: TopoOverlap_{i,t} =  {\sum_j G_{i,j,t} G_{i,j,(t+1)} \over \sqrt{\sum_j G{i,j,t} \sum_j G{i,j,t}}}
+
+    When calc='node', then the topological overlap is the mean of math:`TopoOverlap_{i,t}`:   
+
+    .. math:: \text{AvgTopoOverlap}_{i} = {1 \over T-1} \sum_t TopoOverlap
+
+    where T is the number of time-points. This is called the _average topological overlap_.
+
+    When calc='node', the *temporal-correlation coefficient* is calculated
+
+    .. math:: \text{TempCorrCoeff}_{i,t} = \sum_j {G_{i,j,t} \over G_{i,j,(t+1)}}
+
 
     References
     ----------
     .. [topo-1] Tang et al (2010) Small-world behavior in time-varying graphs. Phys. Rev. E 81, 055101(R) [`arxiv link <https://arxiv.org/pdf/0909.1712.pdf>`_]
     .. [topo-2] Nicosia et al (2013) "Graph Metrics for Temporal Networks" In: Holme P., Saramäki J. (eds) Temporal Networks. Understanding Complex Systems. Springer. 
         [`arxiv link <https://arxiv.org/pdf/1306.0493.pdf>`_]
+
     """
 
     tnet = process_input(tnet, ['C', 'G', 'TO'])[0]
