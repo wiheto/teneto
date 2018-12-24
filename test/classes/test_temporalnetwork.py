73ed6f8cf1d2960a88ec34c9e41438e55585da6b
wiheto <william.thompson@ki.se>
2018-12-23 17:42:07 -0800

    tempnet tests

---
+++ test/classes/test_temporalnetwork.py
@@ -0,0 +1,53 @@
+import teneto 
+import pytest 
+import numpy as np 
+
+def test_errors():
+    # Make sure that only 1 of three different input methods is specified
+    with pytest.raises(ValueError):
+        teneto.TemporalNetwork(from_dict={}, from_array=np.zeros([2,2]))
+    # Make sure error raised from_array if not a numpy array
+    with pytest.raises(ValueError):
+        teneto.TemporalNetwork(from_array=[1,2,3])
+    with pytest.raises(ValueError):
+        teneto.TemporalNetwork(from_array=np.array([2]))
+    # Make sure error raised from_dict if not a dictionary
+    with pytest.raises(ValueError):
+        teneto.TemporalNetwork(from_dict=[1,2,3])
+        # Make sure error raised from_dict if not a dictionary
+    with pytest.raises(ValueError):
+        teneto.TemporalNetwork(from_edgelist='1,2,3')
+    with pytest.raises(ValueError):
+        teneto.TemporalNetwork(from_edgelist=[[0,1],[0,1,2,3]])
+    # Make sure error raised when nettype is wrong
+    with pytest.raises(ValueError):
+        teneto.TemporalNetwork(nettype='s')
+    with pytest.raises(ValueError):
+        teneto.TemporalNetwork(timetype='s')
+
+def test_define_tnet(): 
+    tnet = teneto.TemporalNetwork(nettype='wu', timetype='discrete')
+    assert tnet.network.shape[1] == 4
+    tnet = teneto.TemporalNetwork(nettype='bu')
+    assert tnet.network.shape[1] == 3
+    edgelist = [[0,1,2],[0,2,1]]
+    tnet_edgelist = teneto.TemporalNetwork(from_edgelist=edgelist)
+    assert tnet_edgelist.network.shape == (2,3)
+    G = np.zeros([3,3,3]) 
+    G[[0,0],[1,2],[2,1]] = 1
+    tnet_array = teneto.TemporalNetwork(from_array=G)
+    assert all(tnet_array.network == tnet_edgelist.network)
+    C = teneto.utils.graphlet2contact(G)
+    tnet_dict = teneto.TemporalNetwork(from_dict=C)
+    assert all(tnet_dict.network == tnet_edgelist.network)
+    tnet_edgelist.add_edge([[0,3,1]])
+    assert all(tnet_edgelist.network.iloc[-1].values == [0,3,1])
+    assert tnet_edgelist.network.shape == (3,3)
+
+def test_tnet_functions(): 
+    G = np.zeros([3,3,3]) 
+    G[[0,0],[1,2],[2,1]] = 1
+    G = G + G.transpose([1, 0, 2])
+    tnet = teneto.TemporalNetwork(from_array=G)
+    D = tnet.calc_networkmeasure('temporal_degree_centrality')
+    assert all(G.sum(axis=-1).sum(axis=-1) == D)
