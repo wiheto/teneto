19049766194715fdfbaa70392a976a32603aa822
wiheto <william.thompson@ki.se>
2018-12-24 16:46:48 -0800

    improving docs

--- teneto/plot/circle_plot.py
+++ teneto/plot/circle_plot.py
@@ -36,7 +36,7 @@ def circle_plot(netIn, ax, nlabs=[], linestyle='k-', nodesize=1000):
     >>> j = [3, 4, 5, 5, 4, 5, 5]
     >>> G[i, j] = 1
 
-    .. plot: 
+    .. plot::
         Circle plot example: 
         >>> fig, ax = plt.subplots(1)
         >>> teneto.plot.circle_plot(G, ax)
