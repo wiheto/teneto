2b8fc2ad71a82342748a228e344e3f1a1b734f3a
wiheto <william.thompson@ki.se>
2018-12-26 22:11:53 -0800

    more gennet tests

--- test/test_generatenetwork/test_generatenetwork.py
+++ test/test_generatenetwork/test_generatenetwork.py
@@ -12,6 +12,8 @@ def test_gennet_fail():
         teneto.generatenetwork.rand_binomial([3, 2, 1], [-1])
     with pytest.raises(ValueError):
         teneto.generatenetwork.rand_binomial([2, 2], [0], nettype='bx')
+    with pytest.raises(ValueError):
+        teneto.generatenetwork.rand_binomial([2, 2], [1.2], nettype='bx')
 
 
 def test_gen_randbinomial():
@@ -24,3 +26,6 @@ def test_gen_randbinomial():
     assert G[:, :, -1].max() == 0
     G = teneto.generatenetwork.rand_binomial([3, 1], [1], initialize=3)
     assert G[:, :, 0].max() == 1
+    G = teneto.generatenetwork.rand_binomial([3, 1], (1,1), initialize=0.6666, randomseed=1)
+    assert G[:, :, 0].sum() == 4
+
