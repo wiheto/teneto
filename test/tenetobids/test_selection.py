6152630b361cc64e7b679050bdd07b4ecc73ee4e
wiheto <william.thompson@ki.se>
2019-02-06 16:14:00 -0800

    assert -> AssertionError

--- test/tenetobids/test_selection.py
+++ test/tenetobids/test_selection.py
@@ -5,58 +5,74 @@ def test_define():
     dataset_path = teneto.__path__[0] + '/data/testdata/dummybids/'
     tnet = teneto.TenetoBIDS(
         dataset_path, pipeline='fmriprep', raw_data_exists=False)
-    assert len(tnet.get_selected_files(quiet=1)) == 6
+    if not len(tnet.get_selected_files(quiet=1)) == 6:
+        raise AssertionError()
     tnet = teneto.TenetoBIDS(
         dataset_path, pipeline='fmriprep', raw_data_exists=False)
-    assert len(tnet.get_selected_files(quiet=1, forfile={'sub': '001'})) == 3
+    if not len(tnet.get_selected_files(quiet=1, forfile={'sub': '001'})) == 3:
+        raise AssertionError()
     fname = 'sub-001_task-a_run-beta_bold_preproc.nii.gz'
-    assert len(tnet.get_selected_files(quiet=1, forfile=fname)) == 1
+    if not len(tnet.get_selected_files(quiet=1, forfile=fname)) == 1:
+        raise AssertionError()
 
 
 def test_define_sub_then_task():
     tnet = teneto.TenetoBIDS(teneto.__path__[0] + '/data/testdata/dummybids/',
                              pipeline='fmriprep', bids_tags={'sub': '001'}, raw_data_exists=False)
-    assert len(tnet.get_selected_files(quiet=1)) == 3
+    if not len(tnet.get_selected_files(quiet=1)) == 3:
+        raise AssertionError()
     tnet.set_bids_tags({'task': 'a'})
-    assert len(tnet.get_selected_files(quiet=1)) == 2
+    if not len(tnet.get_selected_files(quiet=1)) == 2:
+        raise AssertionError()
 
 
 def test_define_run_then_sub():
     tnet = teneto.TenetoBIDS(teneto.__path__[0] + '/data/testdata/dummybids/',
                              pipeline='fmriprep', bids_tags={'run': 'alpha'}, raw_data_exists=False)
-    assert len(tnet.get_selected_files(quiet=1)) == 4
+    if not len(tnet.get_selected_files(quiet=1)) == 4:
+        raise AssertionError()
     tnet.set_bids_tags({'sub': '001'})
-    assert len(tnet.get_selected_files(quiet=1)) == 2
+    if not len(tnet.get_selected_files(quiet=1)) == 2:
+        raise AssertionError()
 
 
 def test_define_task_then_run():
     tnet = teneto.TenetoBIDS(teneto.__path__[
                              0] + '/data/testdata/dummybids/', pipeline='fmriprep', bids_tags={'task': 'a'}, raw_data_exists=False)
-    assert len(tnet.get_selected_files(quiet=1)) == 4
+    if not len(tnet.get_selected_files(quiet=1)) == 4:
+        raise AssertionError()
     tnet.set_bids_tags({'run': 'beta'})
-    assert len(tnet.get_selected_files(quiet=1)) == 2
+    if not len(tnet.get_selected_files(quiet=1)) == 2:
+        raise AssertionError()
 
 
 def test_get_pipeline_alternatives():
     tnet = teneto.TenetoBIDS(teneto.__path__[0] + '/data/testdata/dummybids/',
                              pipeline='teneto-tests', bids_tags={'task': 'a'}, raw_data_exists=False)
     pipeline = tnet.get_pipeline_alternatives()
-    assert 'fmriprep' in pipeline
-    assert 'teneto-tests' in pipeline
+    if not 'fmriprep' in pipeline:
+        raise AssertionError()
+    if not 'teneto-tests' in pipeline:
+        raise AssertionError()
 
 
 def test_set_bad_subjects():
     tnet = teneto.TenetoBIDS(teneto.__path__[0] + '/data/testdata/dummybids/', pipeline='teneto-tests',
                              pipeline_subdir='parcellation', bids_tags={'task': 'a'}, raw_data_exists=False)
     tnet.set_bad_subjects('001')
-    assert len(tnet.bad_files) == 2
+    if not len(tnet.bad_files) == 2:
+        raise AssertionError()
     tnet.set_bad_subjects('001', reason='last', oops=True)
-    assert len(tnet.bad_files) == 0
-    assert '001' in tnet.bids_tags['sub']
-    assert len(tnet.bids_tags['sub']) == 2
+    if not len(tnet.bad_files) == 0:
+        raise AssertionError()
+    if not '001' in tnet.bids_tags['sub']:
+        raise AssertionError()
+    if not len(tnet.bids_tags['sub']) == 2:
+        raise AssertionError()
     tnet.set_bad_subjects('001')
     tnet.set_bad_subjects(['002'])
-    assert len(tnet.bad_subjects) == 2
+    if not len(tnet.bad_subjects) == 2:
+        raise AssertionError()
 
 
 def test_print():
--- test/tenetobids/tnet_test.py
+++ test/tenetobids/tnet_test.py
@@ -20,7 +20,8 @@ def test_tnet_derive():
     R_jc = parcdata.transpose().corr().values[0, 1] * -1
     jc = float(tnet.tvc_data_[0][(tnet.tvc_data_[0]['i'] == 0) & (
         tnet.tvc_data_[0]['j'] == 1) & (tnet.tvc_data_[0]['t'] == 0)]['weight'])
-    assert np.round(R_jc, 12) == np.round(jc, 12)
+    if not np.round(R_jc, 12) == np.round(jc, 12):
+        raise AssertionError()
 
 
 def test_make_fc_and_tvc():
@@ -30,8 +31,10 @@ def test_make_fc_and_tvc():
     tnet.load_data('parcellation')
     r = tnet.make_functional_connectivity(returngroup=True)[0, 1]
     fc_files = tnet.get_selected_files(pipeline='functionalconnectivity')
-    assert '_conn.tsv' in fc_files[0]
-    assert len(fc_files) == 1
+    if not '_conn.tsv' in fc_files[0]:
+        raise AssertionError()
+    if not len(fc_files) == 1:
+        raise AssertionError()
     R = tnet.parcellation_data_[0].transpose().corr().values[0, 1]
     tnet.derive_temporalnetwork({'method': 'jackknife', 'dimord': 'node,time',
                                  'postpro': 'standardize'}, update_pipeline=True, confound_corr_report=False)
@@ -58,9 +61,12 @@ def test_make_fc_and_tvc():
                                  'weight-var': 'from-subject-fc'}, update_pipeline=True, confound_corr_report=False)
     tnet.load_data('tvc')
     JCv = tnet.tvc_data_[0].iloc[0].values[-1]
-    assert np.round(JCw, 15) == np.round((JC*r)+R, 15)
-    assert np.round(JCv, 15) == np.round((JC*r), 15)
-    assert np.round(JCm, 15) == np.round((JC)+R, 15)
+    if not np.round(JCw, 15) == np.round((JC*r)+R, 15):
+        raise AssertionError()
+    if not np.round(JCv, 15) == np.round((JC*r), 15):
+        raise AssertionError()
+    if not np.round(JCm, 15) == np.round((JC)+R, 15):
+        raise AssertionError()
 
 
 # def test_communitydetection():
@@ -73,13 +79,23 @@ def test_make_fc_and_tvc():
 #     tnet.set_pipeline('teneto_' + teneto.__version__)
 #     tnet.load_community_data('temporal')
 #     C = np.squeeze(tnet.community_data_)
-#     assert C[0, 0] == C[1, 0] == C[2, 0]
-#     assert C[3, 0] == C[4, 0] == C[5, 0]
-#     assert C[0, 2] == C[1, 2] == C[2, 2] == C[3, 2]
-#     assert C[4, 2] == C[5, 2]
-#     assert C[3, 0] != C[0, 0]
-#     assert C[4, 2] != C[0, 2]
-
+#     if not C[0, 0] == C[1, 0] == C[2, 0]
+#:
+# raise AssertionError()
+#    if not C[3, 0] == C[4, 0] == C[5, 0]
+#:
+# raise AssertionError()
+#    if not C[0, 2] == C[1, 2] == C[2, 2] == C[3, 2]
+#:
+# raise AssertionError()
+#    if not C[4, 2] == C[5, 2]
+#:
+# raise AssertionError()
+#    if not C[3, 0] != C[0, 0]
+#:
+# raise AssertionError()
+#    if not C[4, 2] != C[0, 2]:
+# raise AssertionError(
 
 def test_networkmeasure():
     # calculate and load a network measure
@@ -91,7 +107,8 @@ def test_networkmeasure():
                              raw_data_exists=False)
     tnet.networkmeasures('volatility', {'calc': 'time'}, tag='time')
     tnet.load_data('temporalnetwork', measure='volatility', tag='time')
-    assert tnet.temporalnetwork_data_['volatility'][0].shape == (19, 1)
+    if not tnet.temporalnetwork_data_['volatility'][0].shape == (19, 1):
+        raise AssertionError()
 
 
 # def test_timelockednetworkmeasure():
@@ -104,8 +121,9 @@ def test_networkmeasure():
 #     tnet.make_timelocked_events('volatility', 'testevents', [
 #                                 1], [-1, 1], tag='time')
 #     tnet.load_timelocked_data('volatility')
-#     assert np.all(np.squeeze(tnet.timelocked_data_) ==
-#                   np.squeeze(tnet.networkmeasure_[0, 0:3]))
+#     if not np.all(np.squeeze(tnet.timelocked_data_) == np.squeeze(tnet.networkmeasure_[0, 0:3]))
+#:
+# raise AssertionError()                  
 
 
 def test_tnet_derive_with_removeconfounds():
@@ -115,8 +133,10 @@ def test_tnet_derive_with_removeconfounds():
     # Set the confound pipeline in fmriprep
     tnet.set_confound_pipeline('fmriprep')
     alt = tnet.get_confound_alternatives()
-    assert 'confound1' in alt
-    assert 'confound2' in alt
+    if not 'confound1' in alt:
+        raise AssertionError()
+    if not 'confound2' in alt:
+        raise AssertionError()
     # Set the confounds
     tnet.set_confounds('confound1')
     # Remove confounds
@@ -125,12 +145,13 @@ def test_tnet_derive_with_removeconfounds():
     f = f.replace('.tsv', '.json')
     with open(f) as fs:
         sidecar = json.load(fs)
-    assert 'confoundremoval' in sidecar
+    if not 'confoundremoval' in sidecar:
+        raise AssertionError()
     # Removing below tests due to errors caused by concurrent images.
     #tnet.derive_temporalnetwork({'method': 'jackknife'})
     # Make sure report directory exists
-    #assert os.path.exists(teneto.__path__[0] + '/data/testdata/dummybids/derivatives/teneto_' + teneto.__version__ + '/sub-001/func/tvc/report')
-
+    #if not os.path.exists(teneto.__path__[0] + '/data/testdata/dummybids/derivatives/teneto_' + teneto.__version__ + '/sub-001/func/tvc/report'):
+    raise AssertionError()
 
 def test_tnet_scrubbing():
     tnet = teneto.TenetoBIDS(teneto.__path__[0] + '/data/testdata/dummybids/', pipeline='teneto-tests',
@@ -141,7 +162,8 @@ def test_tnet_scrubbing():
     tnet.load_data('parcellation')
     dat = np.where(np.isnan(np.squeeze(tnet.parcellation_data_[0].values)))
     targ = np.array([[0, 0, 1, 1], [4, 5, 4, 5]])
-    assert np.all(targ == dat)
+    if not np.all(targ == dat):
+        raise AssertionError()
 
 
 def test_tnet_scrubbing_and_spline():
@@ -155,10 +177,11 @@ def test_tnet_scrubbing_and_spline():
     tnet.load_data('parcellation')
     dat_scrub = tnet.parcellation_data_[0].values
     # Make sure there is a difference
-    assert np.sum(dat_scrub != dat_orig)
+    if not np.sum(dat_scrub != dat_orig):
+        raise AssertionError()
     # Show that the difference between the original data at scrubbed time point is larger in data_orig
-    assert np.sum(
-        np.abs(np.diff(dat_orig[0]))-np.abs(np.diff(dat_scrub[0]))) > 0
+    if not np.sum(np.abs(np.diff(dat_orig[0]))-np.abs(np.diff(dat_scrub[0]))) > 0:
+        raise AssertionError()
     # Future tests: test that the cubic spline is correct
 
 
@@ -169,10 +192,12 @@ def test_tnet_set_bad_files():
     tnet.load_data('parcellation')
     tnet.set_confound_pipeline('fmriprep')
     tnet.set_exclusion_file('confound2', '>0.5')
-    assert len(tnet.bad_files) == 1
-    assert tnet.bad_files[0] == tnet.BIDS_dir + 'derivatives/' + tnet.pipeline + \
+    if not len(tnet.bad_files) == 1:
+        raise AssertionError()
+    if not tnet.bad_files[0] == tnet.BIDS_dir + 'derivatives/' + tnet.pipeline + \
         '/sub-001/func/' + tnet.pipeline_subdir + \
-        '/sub-001_task-a_run-alpha_roi.tsv'
+        '/sub-001_task-a_run-alpha_roi.tsv':
+        raise AssertionError()
 
 
 def test_tnet_make_parcellation():
@@ -185,7 +210,8 @@ def test_tnet_make_parcellation():
     tnet.make_parcellation('gordon2014_333')
     tnet.load_data('parcellation')
     # Hard coded facts about dummy data
-    assert tnet.parcellation_data_[0].shape == (2, 333)
+    if not tnet.parcellation_data_[0].shape == (2, 333):
+        raise AssertionError()
 
 
 def test_tnet_checksidecar():
@@ -198,8 +224,10 @@ def test_tnet_checksidecar():
     with open(teneto.__path__[0] + '/data/testdata/dummybids/derivatives/teneto_' + teneto.__version__ + '/sub-001/func/parcellation/sub-001_task-a_run-alpha_roi.json') as fs:
         sidecar = json.load(fs)
     # Check both steps are in sidecar
-    assert 'parcellation' in sidecar.keys()
-    assert 'scrubbed_timepoints' in sidecar.keys()
+    if not 'parcellation' in sidecar.keys():
+        raise AssertionError()
+    if not 'scrubbed_timepoints' in sidecar.keys():
+        raise AssertionError()
 
 
 def test_tnet_io():
@@ -209,7 +237,8 @@ def test_tnet_io():
                        '/data/testdata/dummybids/teneosave.pkl')
     tnet2 = teneto.TenetoBIDS.load_frompickle(
         teneto.__path__[0] + '/data/testdata/dummybids/teneosave.pkl')
-    assert tnet2.get_selected_files() == tnet.get_selected_files()
+    if not tnet2.get_selected_files() == tnet.get_selected_files():
+        raise AssertionError()
 
 
 def test_tnet_scrubbing_and_exclusion_options():
