59c0c8fe3fe39a0b1131582d6bef601d7c53b046
granitz <47792053+granitz@users.noreply.github.com>
2019-02-20 10:05:09 -0800

    Update __init__.py

--- teneto/temporalcommunity/__init__.py
+++ teneto/temporalcommunity/__init__.py
@@ -4,3 +4,5 @@ Functions to quantify temporal communities
 
 from .flexibility import flexibility
 from .allegiance import allegiance
+from .recruitment import recruitment
+from .integration import recruitment
