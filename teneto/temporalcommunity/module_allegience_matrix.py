7a9efb7af8f1a95aacbc7ae199691eec79403cb5
William Hedley Thompson <wiheto@users.noreply.github.com>
2019-02-19 21:13:42 -0800

    Update module_allegience_matrix.py
    Fixing tab issues that were causing codacy errors

--- teneto/temporalcommunity/module_allegience_matrix.py
+++ teneto/temporalcommunity/module_allegience_matrix.py
@@ -1,37 +1,35 @@
 def module_allegience_matrix(community):
-	"""
-	Computes the module allegiance matrix with values representing the probability that
-	nodes i and j were assigned to the same community by time-varyin clustering methods. 
+    """
+    Computes the module allegiance matrix with values representing the probability that
+    nodes i and j were assigned to the same community by time-varying clustering methods. 
 
-	parameters: 
-		community : array
-			array of community assignment of size node,time
+    parameters
+    ----------
+    community : array
+	array of community assignment of size node,time
 
-	returns
-	-------
-		P : array
-			module allegiance matrix, with P_ij probability that area i and j are in the same community
+    returns
+    -------
+    P : array
+	module allegiance matrix, with P_ij probability that area i and j are in the same community
 
-	
-	Reference:
-	---------- 
-	Bassett, et al. (2013) “Robust detection of dynamic community structure in networks”, 
-	Chaos, 23, 1 
+    Reference:
+    ---------- 
+    Bassett, et al. (2013) “Robust detection of dynamic community structure in networks”, Chaos, 23, 1 
+    """ 	
+    N = len(community)
+    C = np.unique(community)
+    T = P = np.zeros((N,N))
 
-	""" 
-  N = len(community)
-  C = np.unique(community)
-	T = P = np.zeros((N,N))
+    for t in range(len(community[0,:])):
+	for i in range(len(community[:,0])):
+	    for j in range(len(community[:,0])):
+	        if i==j:
+		    continue
+		# T_ij indicates the number of times that i and j are assigned to the same community across time
+		if community[i][t] == community[j][t]:
+		    T[i,j] +=1 
 
-	for t in range(len(community[0,:])):
-		for i in range(len(community[:,0])):
-			for j in range(len(community[:,0])):
-				if i==j:
-					continue
-				# T_ij indicates the number of times that i and j are assigned to the same community across time
-				if community[i][t] == community[j][t]:
-					T[i,j] +=1 
+    P = (1/C)*T # module allegiance matrix, probability that ij were assigned to the same community 
 
-	P = (1/C)*T # module allegiance matrix, probability that ij were assigned to the same community 
-
-  return P
+    return P
