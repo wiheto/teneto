8bcb860de03416f7681d7f3961ccdf643f2e3faf
granitz <47792053+granitz@users.noreply.github.com>
2019-02-20 07:27:07 -0800

    Create test_allegiance.py

---
+++ test/temporalcommunity/test_allegiance.py
@@ -0,0 +1,29 @@
+import numpy as np
+from teneto.temporalcommunity import allegiance
+
+def test_allegiance():
+
+    np.random.seed(10)
+    # Two graphlets templates with definite community structure
+    a = np.array([[1, 1, 1, 0, 0, 0], [1, 1, 1, 0, 0, 0], [1, 1, 1, 0, 0, 0], [
+                 0, 0, 0, 1, 1, 1], [0, 0, 0, 1, 1, 1], [0, 0, 0, 1, 1, 1]])
+    b = np.array([[1, 1, 1, 1, 0, 0], [1, 1, 1, 1, 0, 0], [1, 1, 1, 1, 0, 0], [
+                 1, 1, 1, 1, 0, 0], [0, 0, 0, 0, 1, 1], [0, 0, 0, 0, 1, 1]])
+    # Make into 4 time points
+    G = np.stack([a, a, b, b]).transpose([1, 2, 0])
+
+    # returns node,time array of community assignments
+    community = teneto.communitydetection.temporal_louvain(
+        G, intersliceweight=0.1, n_iter=1)
+
+    N = len(community)
+    C = len(community[0])
+
+    P = allegience(N,C,community)
+
+    if not P[0,1] == P[0,2] == P[1,0] == P[1,2] == P[2,0] == P[2,1] == P[4,5] == P[5,4] == 1.0:
+        raise AssertionError()
+    if not P[0,3] == P[1,3] == P[2,3] == P[4,3] == P[5,3] == 0.5:
+        raise AssertionError()
+    if not P[3,0] == P[3,1] == P[3,2] == P[3,4] == P[3,5] == 0.5:
+        raise AssertionError()
