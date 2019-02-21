63dd62120405c4cca460f6e9dc0848647b0e9b85
William Hedley Thompson <wiheto@users.noreply.github.com>
2019-02-20 22:18:23 -0800

    Update integration.py

--- teneto/temporalcommunity/integration.py
+++ teneto/temporalcommunity/integration.py
@@ -1,6 +1,10 @@
-def integration(temporalcommunities,staticcommunities):
+import numpy as np
+from .allegiance import allegiance
+
+
+def integration(temporalcommunities, staticcommunities):
     """
-    Calculates the integration coefficient for each node. Measures the average probability 
+    Calculates the integration coefficient for each node. Measures the average probability
     that a node is in the same community as nodes from other systems.
 
       Parameters:
@@ -12,21 +16,21 @@ def integration(temporalcommunities,staticcommunities):
 
       Returns:
       -------
-      Rcoeff : array
+      Icoeff : array
           integration coefficient for each node
 
-    References: 
+    References:
     ----------
-    Danielle S. Bassett, Muzhi Yang, Nicholas F. Wymbs, Scott T. Grafton. 
-    Learning-Induced Autonomy of Sensorimotor Systems. Nat Neurosci. 2015 May;18(5):744-51. 
+    Danielle S. Bassett, Muzhi Yang, Nicholas F. Wymbs, Scott T. Grafton.
+    Learning-Induced Autonomy of Sensorimotor Systems. Nat Neurosci. 2015 May;18(5):744-51.
 
-    Marcelo Mattar, Michael W. Cole, Sharon Thompson-Schill, Danielle S. Bassett. 
-    A Functional Cartography of Cognitive Systems. PLoS Comput Biol. 2015 Dec 
+    Marcelo Mattar, Michael W. Cole, Sharon Thompson-Schill, Danielle S. Bassett.
+    A Functional Cartography of Cognitive Systems. PLoS Comput Biol. 2015 Dec
     2;11(12):e1004533.
     """
-    
+
     # make sure the static and temporal communities have the same number of nodes
-    if np.shape[0] != temporalcommunities.shape[0]:
+    if staticcommunities.shape[0] != temporalcommunities.shape[0]:
         raise ValueError(
             'Temporal and static communities have different dimensions')
 
