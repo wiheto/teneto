620d8eb32d974ab3f9a23dcbf10ba79518bcf3de
wiheto <william.thompson@ki.se>
2019-07-05 18:55:24 -0700

    updating test

--- test/classes/test_workflow.py
+++ test/classes/test_workflow.py
@@ -14,15 +14,15 @@ def test_workflow_temporalnetwork():
     Dmag = teneto.networkmeasures.temporal_degree_centrality(Gmag)
     twf = TenetoWorkflow()
     twf.add_node('network_create', 'TemporalNetwork',
-                 func_params={'from_array': G})
-    twf.add_node('binarize_percent', 'binarize', func_params={
+                 params={'from_array': G})
+    twf.add_node('binarize_percent', 'binarize', params={
                  'threshold_type': 'percent', 'threshold_level': 0.05})
     twf.add_node('degree_th-percent', 'calc_networkmeasure',
-                 func_params={'networkmeasure': 'temporal_degree_centrality'})
+                 params={'networkmeasure': 'temporal_degree_centrality'})
     twf.add_node('binarize_magnitude', 'binarize', depends_on='network_create',
-                 func_params={'threshold_type': 'magnitude', 'threshold_level': 0})
+                 params={'threshold_type': 'magnitude', 'threshold_level': 0})
     twf.add_node('degree_th-magnitude', 'calc_networkmeasure', depends_on='binarize_magnitude',
-                 func_params={'networkmeasure': 'temporal_degree_centrality'})
+                 params={'networkmeasure': 'temporal_degree_centrality'})
     twf.run()
     twf.make_workflow_figure()
     if not all(Dth == twf.output_['degree_th-percent']):
