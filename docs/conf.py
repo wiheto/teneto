e30f58707cc2b14bebc3d025b9096ef5a23612c5
wiheto <william.thompson@ki.se>
2019-07-17 21:39:09 -0700

    theme

--- docs/conf.py
+++ docs/conf.py
@@ -90,7 +90,22 @@ pygments_style = 'sphinx'
 html_theme = 'default'
 
 
-
+html_theme_options = {
+    'canonical_url': '',
+    #'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 3,
+    'includehidden': True,
+    'titles_only': False
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
