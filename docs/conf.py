dca1bbde26be1c69a5ebcf56b2283ffa1fe0e515
wiheto <william.thompson@ki.se>
2019-11-12 20:58:45 -0800

    path fix

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ project = 'teneto'
 copyright = '2018, William Hedley Thompson'
 author = 'William Hedley Thompson'
 
-version = "../teneto/_version.py"
+version = "teneto/_version.py"
 verstrline = open(version, "rt").read()
 version = verstrline.split('"')[1].split('-')[0]
 release = verstrline.split('"')[1]
