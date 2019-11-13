b03752d761f5c034b66973951f7dd01a31e6169b
wiheto <william.thompson@ki.se>
2019-11-12 22:09:55 -0800

    style changes

--- teneto/networkmeasures/temporal_betweenness_centrality.py
+++ teneto/networkmeasures/temporal_betweenness_centrality.py
@@ -1,6 +1,4 @@
-"""
-networkmeasures.temporal_closeness_centrality
-"""
+"""Calculates temporal betweenness centrality"""
 
 import numpy as np
 from .shortest_temporal_path import shortest_temporal_path
@@ -51,9 +49,9 @@ def temporal_betweenness_centrality(tnet=None, paths=None, calc='pertime'):
     Teneto returns a normalized betweenness centrality value,
     defined as [Bet-1]_:
 
-    .. math:: 
-        
-        B_it = {1 \over (N-1)(N-2)} \sum_{j = 1; j \neq i} 
+    .. math::
+
+        B_it = {1 \over (N-1)(N-2)} \sum_{j = 1; j \neq i}
         \sum_{k = 1; k \neq i,j} {\sigma^i_{jkt} \over \sigma_{jk}}
 
     If a temporal network is used as input (i.e. not the paths), then teneto
@@ -67,10 +65,7 @@ def temporal_betweenness_centrality(tnet=None, paths=None, calc='pertime'):
 
     .. [Bet-1] Tang
 
-
-
     """
-
     if tnet is not None and paths is not None:
         raise ValueError('Only network or path input allowed.')
     if tnet is None and paths is None:
