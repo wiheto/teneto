f49667df0333dc6facc5e68842a2e28cae7c467d
wiheto <william.thompson@ki.se>
2019-11-14 21:32:30 -0800

    style

--- teneto/timeseries/__init__.py
+++ teneto/timeseries/__init__.py
@@ -9,3 +9,10 @@ from .postprocess import postpro_standardize
 from .postprocess import postpro_boxcox
 from .remove_confounds import remove_confounds
 from .report import gen_report
+__all__ = ['derive_temporalnetwork',
+           'postpro_pipeline',
+           'postpro_fisher',
+           'postpro_standardize',
+           'postpro_boxcox',
+           'remove_confounds',
+           'gen_report']
