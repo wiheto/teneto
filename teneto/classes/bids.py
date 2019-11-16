c68bc1a0f171c5b6d701776a97171e80df7e470e
wiheto <william.thompson@ki.se>
2019-11-16 14:53:57 -0800

    fixing import probelm

--- teneto/classes/bids.py
+++ teneto/classes/bids.py
@@ -4,7 +4,7 @@ import bids
 import numpy as np
 import inspect
 import json
-from teneto.neuroimagingtools import load_tabular_file, get_bids_tag, get_sidecar, confound_matching, process_exclusion_criteria, drop_bids_suffix, make_directories
+from ..neuroimagingtools import load_tabular_file, get_bids_tag, get_sidecar, confound_matching, process_exclusion_criteria, drop_bids_suffix, make_directories
 import pandas as pd
 from . import TemporalNetwork
 
