95312065f636a1ab260b3bc2c684c0b5ea89cf0f
wiheto <william.thompson@ki.se>
2019-11-17 13:39:44 -0800

    removing old tenetobids utils

--- teneto/neuroimagingtools/__init__.py
+++ teneto/neuroimagingtools/__init__.py
@@ -2,12 +2,10 @@
 
 from .fmriutils import make_parcellation
 from .bidsutils import make_directories, drop_bids_suffix, \
-    get_bids_tag, load_tabular_file, \
-    get_sidecar, confound_matching, \
+    load_tabular_file, get_sidecar, \
     process_exclusion_criteria, \
     censor_timepoints, exclude_runs
 
-__all__ = ['make_parcellation', 'make_directories', 'drop_bids_suffix',
-           'get_bids_tag', 'load_tabular_file',
-           'get_sidecar', 'confound_matching',
+__all__ = ['make_parcellation', 'make_directories',
+           'load_tabular_file', 'get_sidecar', 'confound_matching',
            'process_exclusion_criteria', 'exclude_runs', 'censor_timepoints']
