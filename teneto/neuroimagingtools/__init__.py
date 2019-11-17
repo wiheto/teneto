0c0a9a4cffa2c0fece97624cbe1596622d10d2a7
wiheto <william.thompson@ki.se>
2019-11-17 13:40:27 -0800

    removing old tenetobids utils

--- teneto/neuroimagingtools/__init__.py
+++ teneto/neuroimagingtools/__init__.py
@@ -6,6 +6,6 @@ from .bidsutils import make_directories, drop_bids_suffix, \
     process_exclusion_criteria, \
     censor_timepoints, exclude_runs
 
-__all__ = ['make_parcellation', 'make_directories',
-           'load_tabular_file', 'get_sidecar', 'confound_matching',
-           'process_exclusion_criteria', 'exclude_runs', 'censor_timepoints']
+__all__ = ['make_parcellation', 'make_directories', 'drop_bids_suffix',
+           'load_tabular_file', 'get_sidecar', 'process_exclusion_criteria',
+           'exclude_runs', 'censor_timepoints']
