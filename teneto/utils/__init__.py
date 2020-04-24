160e105a0c0f55e5fa13526e45bbe0f68884e177
wiheto <william.thompson@ki.se>
2020-04-24 09:25:28 +0200

    adding missing comma

--- teneto/utils/__init__.py
+++ teneto/utils/__init__.py
@@ -8,7 +8,7 @@ from .utils import graphlet2contact, contact2graphlet,\
     df_to_array, check_distance_funciton_input,\
     create_traj_ranges, get_dimord, get_network_when,\
     create_supraadjacency_matrix, check_TemporalNetwork_input,\
-    df_drop_ij_duplicates is_jsonable
+    df_drop_ij_duplicates, is_jsonable
 from .io import tnet_to_nx
 __all__ = ['graphlet2contact', 'contact2graphlet',
            'binarize_percent', 'binarize_rdp', 'binarize_magnitude',
